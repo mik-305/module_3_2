172bbe7375dfb2ff49f153f93892656e1d3946f8
mik-305 <mik-305@bk.ru>
2024-08-12 15:05:49 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,22 +1,26 @@
-def send_email(message, recipient, *,sender = 'university.help@gmail.com'):
+def send_email(message, recipient, *, sender='university.help@gmail.com'):
     if not ('@' in recipient and '@' in sender):
-        print('Невозможно отправить письмо с адреса ',sender,' на адрес ', recipient)   # Неверный формат адреса
+        print('1 Невозможно отправить письмо с адреса ', sender, ' на адрес ', recipient)  # Неверный формат адреса
+        return
+    elif not (recipient[len(recipient) - 4:] == '.com' or recipient[len(recipient) - 3:] == '.ru' or recipient[
+                                                                                                     len(recipient) - 4:] == '.net'):
+        print('2 Невозможно отправить письмо с адреса ', sender, ' на адрес ', recipient)  # Неверный формат адреса
+        return
+    elif not (sender[len(sender) - 4:] == '.com' or sender[len(sender) - 3:] == '.ru' or sender[
+                                                                                         len(sender) - 4:] == '.net'):
+        print('3 Невозможно отправить письмо с адреса ', sender, ' на адрес ', recipient)  # Неверный формат адреса
         return
     else:
-        if recipient == sender:                                                      # Совпадение отправителя и получателя
-            print('Нельзя отправить письмо самому себе! ')
+        if recipient == sender:  # Совпадение отправителя и получателя
+            print('Нельзя отправить письмо самому себе! ', ' Отправитель: ', sender, '  Адресат: ', recipient)
             return
         else:
             if sender == 'university.help@gmail.com':
-                print('Письмо успешно отправлено с адреса ',sender, ' на адрес ', recipient)
+                print('Письмо успешно отправлено с адреса ', sender, ' на адрес ', recipient)
                 return
             else:
-                print('Нестандартный отправитель - ',sender)
-
+                print('Нестандартный отправитель - ', sender)
 
 
 print('---------------------------------------------------------------------------')
-#send_email('Это сообщение для проверки связи!', 'ABBB@CCC.ru')
-send_email('Это сообщение для проверки связи!', 'university.help@gmail.ru',sender = 'university.help@gmail.com')
-
-
+send_email('Это сообщение для проверки связи!', 'abc@viva.ru', sender='university.help@gmail.com')
